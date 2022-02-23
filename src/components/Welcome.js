7046b9409f6a9ea03df58d50c3cac3ef5d6e51e5
Anniina Aarnio <anniinaaarnio@Anniinas-MacBook-Pro.local>
2022-02-23 09:11:08 +0200

    css and firebase backend connection

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Welcome = ({ handleLogout }) => {
   return (
     <section className="welcome">
-      <h1>Welcome!!!</h1>
+      <h1>Welcome to Visma!!</h1>
       <button onClick={handleLogout}>Logout</button>
     </section>
   )
